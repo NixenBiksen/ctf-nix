565c0f82b5a5bd10498ee2d4fdf3ac4c8691e258
Tethys Svensson <freaken@freaken.dk>
2025-05-20 09:54:25 +0200

    Update stuff

--- seccomp-tools/gemset.nix
+++ seccomp-tools/gemset.nix
@@ -1,20 +1,20 @@
 {
   os = {
-    groups = [ "default" ];
-    platforms = [ ];
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
+      remotes = ["https://rubygems.org"];
       sha256 = "0gwd20smyhxbm687vdikfh1gpi96h8qb1x28s2pdcysf6dm6v0ap";
       type = "gem";
     };
     version = "1.1.4";
   };
   seccomp-tools = {
-    dependencies = [ "os" ];
-    groups = [ "default" ];
-    platforms = [ ];
+    dependencies = ["os"];
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
+      remotes = ["https://rubygems.org"];
       sha256 = "0v5zyby5glih0prddxm8wp6gn2glrnvf7y4r64k4iqfpdazdpsa3";
       type = "gem";
     };
