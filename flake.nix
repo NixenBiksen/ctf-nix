740ac8d56badcb98b111fdf38869e9f658926b61
Kristoffer Søholm <k.soeholm@gmail.com>
2025-10-04 17:48:04 +0200

    Export pwndbg separately

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     pwndbg.url = "github:pwndbg/pwndbg";
+    pwndbg.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -10,23 +11,23 @@
       self,
       nixpkgs,
       flake-utils,
-      pwndbg,
-    }:
+      ...
+    }@flakeArgs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         seccomp-tools = pkgs.callPackage ./seccomp-tools { };
         ctf-python = pkgs.callPackage ./ctf-python.nix { };
+        pwndbg = flakeArgs.pwndbg.packages."${system}".pwndbg;
         ctf-env = pkgs.callPackage ./ctf-env.nix {
-          inherit ctf-python seccomp-tools;
-          pwndbg = pwndbg.packages."${system}".pwndbg;
+          inherit ctf-python seccomp-tools pwndbg;
         };
       in
       {
         checks = { inherit seccomp-tools ctf-python ctf-env; };
-        packages = rec {
-          inherit seccomp-tools ctf-python ctf-env;
+        packages = {
+          inherit seccomp-tools ctf-python ctf-env pwndbg;
           default = ctf-env;
         };
       }
